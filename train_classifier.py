c8407b7f7c0fa181e106768eacb00bcff6d5524c
Isaac Osiemo <zacck@Isaacs-MacBook-Pro.local>
2017-03-09 17:27:29 +0200

    work on addind features for classifier training and labels

---
+++ train_classifier.py
@@ -0,0 +1,5 @@
+import sklearn
+#feature contains the inputs to classifier
+features = [[140, "smooth"], [130 , "smooth"], [150, "bumpy"], [170, "bumpy"]]
+#these are the likely outputs we want from the classifier
+labels =["apple", "apple", "orange", "orange"]
