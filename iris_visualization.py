3db58a336ccca04fbf2fa32885331b8d6e421899
zacck <coderv63@gmail.com>
2017-03-11 06:13:31 +0200

    visualize tree and install deps

--- iris_visualization.py
+++ iris_visualization.py
@@ -24,3 +24,16 @@ print 'expected answers'
 print test_target
 print clf.predict(test_data)
 #visualize
+from sklearn.externals.six import StringIO
+import pydotplus
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                        out_file=dot_data,
+                        feature_names=iris.feature_names,
+                        class_names=iris.target_names,
+                        filled=True,
+                        rounded=True,
+                        impurity=False)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf('iris.pdf')
